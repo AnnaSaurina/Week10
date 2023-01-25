86bfa2f7fe56463959745193ed2e67c792579192
AnnaSaurina <the_happy_ann@mail.ru>
2023-01-25 15:27:14 +0300

    Save

---
+++ calculator.js
@@ -0,0 +1,33 @@
+// Сложение
+function sum() {
+    let one = document.getElementById(`number1`);
+    let two = document.getElementById(`number2`);
+    document.writeln(Number(one) + Number(two));
+}
+
+// Вычитание
+function deduct() {
+    let one = document.getElementById(`number1`);
+    let two = document.getElementById(`number2`);
+    document.writeln(Number(one) - Number(two));
+}
+
+// Умножение
+function multiply() {
+    let one = document.getElementById(`number1`);
+    let two = document.getElementById(`number2`);
+    document.writeln(Number(one) * Number(two));
+}
+
+// Деление
+function divide() {
+    let one = document.getElementById(`number1`);
+    let two = document.getElementById(`number2`);
+    document.writeln(Number(one) / Number(two));
+}
+
+// Итог
+function calculate(){
+
+    document.writeln
+}
---
+++ page_name.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    let name = prompt("Как тебя зовут?", "");
+    alert(`Привет, ${name}!`);
+    alert(message);
+}
+
+function showMessage(){
+document.getElementById(`number1`);
+}
