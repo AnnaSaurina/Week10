08ca289887b50779909718a0c1100d8ad3a31c78
AnnaSaurina <the_happy_ann@mail.ru>
2023-02-02 21:03:51 +0300

    Save gallery

---
+++ gallery.js
@@ -0,0 +1,10 @@
+let showPrevious = () => {
+    let image = document.getElementById("image");
+    image.src = '/2.jpg';
+    console.log(image);
+}
+let showNext = () => {
+    let image = document.getElementById("image");
+    image.src = '/3.jpg';
+    console.log(image);
+}
