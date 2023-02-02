137fcf78dfe6d14b9f36f1705d88b9809d2406b3
AnnaSaurina <the_happy_ann@mail.ru>
2023-02-02 20:48:20 +0300

    Save work

--- page_name.js
+++
@@ -1,9 +0,0 @@
-function showMessage() {
-    let name = prompt("Как тебя зовут?", "");
-    alert(`Привет, ${name}!`);
-    alert(message);
-}
-
-function showMessage(){
-document.getElementById(`number1`);
-}
